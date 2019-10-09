8eb0fcfbcf10c5c25565a259be198dd1f7428a2c
Simas2006 <sjuknelis@gmail.com>
2019-10-09 10:21:01 -0400

    Add basic youtube playlist cue-er

---
+++ public/script.js
@@ -0,0 +1,32 @@
+var list = [
+  "Hf1NDb_Hc60",
+  "-zp6nLiqtHs",
+  "OSs6xiXON4s",
+  "llgUV4pNwQs"
+];
+var nextIndex = 0;
+
+var player;
+function onYouTubePlayerAPIReady() {
+  player = new YT.Player('player', {
+    width: '640',
+    height: '390',
+    videoId: list[nextIndex],
+    events: {
+      onReady: onPlayerReady,
+      onStateChange: onPlayerStateChange
+    }
+  });
+  nextIndex++;
+}
+
+function onPlayerReady(event) {
+  event.target.playVideo();
+}
+
+function onPlayerStateChange(event) {
+  if ( event.data == YT.PlayerState.ENDED ) {
+    player.loadVideoById(list[nextIndex],0);
+    nextIndex++;
+  }
+}
