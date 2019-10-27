1625e6c9856acdb3b9362cbf2f807692b88e010a
Simas2006 <sjuknelis@gmail.com>
2019-10-27 12:47:29 -0400

    Implement voting

--- public/home/script.js
+++ public/home/script.js
@@ -1,42 +1,55 @@
-var socket = io();
-var songs;
+var socket,songs,voteTable;
 
 function renderSongs() {
   var table = document.getElementById("votableSongs");
-  for ( var i = 0; i < songs.length; i++ ) {
+  while ( table.firstChild ) {
+    table.removeChild(table.firstChild);
+  }
+  var ids = Object.keys(songs);
+  for ( var i = 0; i < ids.length; i++ ) {
     var row = document.createElement("tr");
     var col1 = document.createElement("td");
     var upArrow = document.createElement("button");
     upArrow.className = "upArrow";
+    if ( voteTable[ids[i]] == 1 ) upArrow.classList.add("checked");
+    upArrow["data-id"] = ids[i];
+    upArrow.onclick = function() {
+      voteSong(this["data-id"],1);
+    }
     col1.appendChild(upArrow);
     var voteIndicator = document.createElement("p");
     voteIndicator.className = "voteIndicator";
-    voteIndicator.innerText = songs[i].votes;
+    voteIndicator.innerText = songs[ids[i]].votes;
     col1.appendChild(voteIndicator);
     var downArrow = document.createElement("button");
     downArrow.className = "downArrow";
+    if ( voteTable[ids[i]] == -1 ) downArrow.classList.add("checked");
+    downArrow["data-id"] = ids[i];
+    downArrow.onclick = function() {
+      voteSong(this["data-id"],-1);
+    }
     col1.appendChild(downArrow);
     row.appendChild(col1);
     var col2 = document.createElement("td");
     var link = document.createElement("a");
-    link.innerText = songs[i].title;
-    link.href = "https://youtube.com/watch?v=" + songs[i].url;
+    link.innerText = songs[ids[i]].title;
+    link.href = "https://youtube.com/watch?v=" + songs[ids[i]].url;
     link.target = "_blank";
     col2.appendChild(link);
     col2.appendChild(document.createElement("br"));
     var sub = document.createElement("sub");
-    sub.innerText = "By " + songs[i].author;
+    sub.innerText = "By " + songs[ids[i]].author;
     col2.appendChild(sub);
     row.appendChild(col2);
     var col3 = document.createElement("td");
     var playButton = document.createElement("a");
     playButton.className = "playButton";
-    playButton.href = "https://youtube.com/watch?v=" + songs[i].url;
+    playButton.href = "https://youtube.com/watch?v=" + songs[ids[i]].url;
     playButton.target = "_blank";
     col3.appendChild(playButton);
     var sub = document.createElement("a");
     sub.innerText = "Watch on YouTube";
-    sub.href = "https://youtube.com/watch?v=" + songs[i].url;
+    sub.href = "https://youtube.com/watch?v=" + songs[ids[i]].url;
     sub.target = "_blank";
     col3.appendChild(sub);
     row.appendChild(col3);
@@ -44,7 +57,31 @@ function renderSongs() {
   }
 }
 
+function voteSong(id,vote) {
+  /* Based on this grid:
+    Want on top, current on side
+    |    | -1 |  0 | +1 |
+    | -1 | +1 | XX | +2 |
+    |  0 | -1 | XX | +1 |
+    | +1 | -2 | XX | -1 |
+  */
+  var magnitudeTable = [
+    [ 1, 0, 2],
+    [-1, 0, 1],
+    [-2, 0,-1]
+  ];
+  var currentVal = voteTable[id] || 0;
+  var magnitude = magnitudeTable[currentVal + 1][vote + 1];
+  if ( voteTable[id] != vote ) voteTable[id] = vote;
+  else voteTable[id] = 0;
+  localStorage.setItem("vote",JSON.stringify(voteTable));
+  socket.emit("vote-song",id,magnitude);
+}
+
 function setupHandlers() {
+  if ( ! localStorage.getItem("vote") ) localStorage.setItem("vote","{}");
+  voteTable = JSON.parse(localStorage.getItem("vote"));
+  socket = io();
   socket.on("get-songs",function(data) {
     songs = data;
     renderSongs();
--- server.js
+++ server.js
@@ -14,29 +14,39 @@ io.on("connection",function(socket) {
     if ( err ) throw err;
     socket.emit("get-songs",JSON.parse(data.toString()));
   });
+  socket.on("vote-song",function(id,magnitude) {
+    if ( magnitude < -2 || magnitude > 2 ) return;
+    if ( ! songFile[id] ) return;
+    songFile[id].votes += magnitude;
+    songFileDirty = true;
+    socket.emit("get-songs",songFile);
+  });
 });
 
-function saveToFile() {
+function saveToFile(callback) {
   console.log("Saving to file...");
   fs.writeFile(__dirname + "/videos.json",JSON.stringify(songFile),function(err) {
     if ( err ) throw err;
     songFileDirty = false;
+    callback();
   });
 }
 
 fs.readFile(__dirname + "/videos.json",function(err,data) {
+  if ( err ) throw err;
   songFile = JSON.parse(data.toString());
 });
 
 process.on("SIGINT",function() {
-  saveToFile();
-  process.exit();
+  saveToFile(function() {
+    process.exit();
+  });
 });
 
 http.listen(PORT,function() {
   console.log("Listening on port " + PORT);
   songFileDirty = true;
   setInterval(function() {
-    if ( songFileDirty ) saveToFile();
+    if ( songFileDirty ) saveToFile(Function.prototype);
   },600000);
 });
